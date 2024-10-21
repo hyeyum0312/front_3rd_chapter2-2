2825a1c9e909b2fd237fe12c5172c0dfe96ade68
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-22 01:21:59 +0900

    feat: cartUtil 계산함수, 할인, 재고

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,74 @@
-import { CartItem, Coupon } from "../../../types";
-
-export const calculateItemTotal = (item: CartItem) => {
-  return 0;
-};
+import { CartItem, Coupon, Discount, Product } from "../../../types";
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const {
+    product: { discounts },
+    quantity,
+  } = item;
+
+  const discountRate = discounts.reduce((maxDiscount, d) => {
+    return quantity >= d.quantity && d.rate > maxDiscount ? d.rate : maxDiscount;
+  }, 0);
+
+  return discountRate;
+};
+
+export const calculateItemTotal = (item: CartItem) => {
+  const discount = getMaxApplicableDiscount(item);
+
+  const {
+    product: { price },
+    quantity,
+  } = item;
+
+  return price * quantity * (1 - discount);
 };
 
 export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+  let totalBeforeDiscount = 0;
+  let totalAfterDiscount = 0;
+
+  cart.forEach((item) => {
+    const { price } = item.product;
+    const { quantity } = item;
+
+    totalBeforeDiscount += price * quantity;
+    totalAfterDiscount += calculateItemTotal(item);
+  });
+
+  let totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
+  // 쿠폰 적용
+  if (selectedCoupon) {
+    if (selectedCoupon.discountType === "amount") {
+      totalAfterDiscount = Math.max(0, totalAfterDiscount - selectedCoupon.discountValue);
+    } else {
+      totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
+    }
+    totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+  }
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount: Math.round(totalBeforeDiscount),
+    totalAfterDiscount: Math.round(totalAfterDiscount),
+    totalDiscount: Math.round(totalDiscount),
   };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (prevCart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
+  return prevCart
+    .map((item) => {
+      if (item.product.id === productId) {
+        const maxQuantity = item.product.stock;
+        const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+        return updatedQuantity > 0 ? { ...item, quantity: updatedQuantity } : null;
+      }
+      return item;
+    })
+    .filter((item): item is CartItem => item !== null);
+};
+
+export const getRemainingStock = (cart: CartItem[], product: Product) => {
+  const cartItem = cart.find((item) => item.product.id === product.id);
+  return product.stock - (cartItem?.quantity || 0);
 };
