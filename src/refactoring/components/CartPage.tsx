7de44aca33ed60312bc51c7bea28ce49e29bb8a4
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-22 01:17:27 +0900

    feat: getMaxDiscount 계산함수 추가

--- src/refactoring/components/CartPage.tsx
+++ src/refactoring/components/CartPage.tsx
@@ -24,6 +24,11 @@ export const CartPage = ({ products, coupons }: Props) => {
     return appliedDiscount;
   };
 
+  // 계산
+  const getMaxDiscount = (discounts: { quantity: number; rate: number }[]) => {
+    return discounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
+  };
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">장바구니</h1>
