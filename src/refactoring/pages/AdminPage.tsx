ac96e0c55a8ab28b2a93f32ac7f038c45501b1ae
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-24 00:33:52 +0900

    refactor: AdminPage  컴포넌트 변경

--- src/refactoring/pages/AdminPage.tsx
+++ src/refactoring/pages/AdminPage.tsx
@@ -303,6 +303,7 @@ export const AdminPage = ({
             ))}
           </div>
         </div>
+
         <div>
           <h2 className="text-2xl font-semibold mb-4">쿠폰 관리</h2>
           <div className="bg-white p-4 rounded shadow">
