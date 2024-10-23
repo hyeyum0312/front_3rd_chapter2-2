b0e277c17d78f9cec4c9ab3265d31bb2c24bab5d
hyeyum0312 <hyeyum0312@gmail.com>
2024-10-23 17:00:10 +0900

    fix: Button컴포넌트변경

--- src/refactoring/components/Cart/CartItemList.tsx
+++ src/refactoring/components/Cart/CartItemList.tsx
@@ -3,36 +3,32 @@ import { getAppliedDiscount } from "../../hooks/utils/cartUtils";
 import { Button } from "../Common/Button";
 
 interface Props {
-  cart: CartItem[];
+  key: string;
+  cartItem: CartItem;
   removeFromCart: (productId: string) => void;
   updateQuantity: (productId: string, newQuantity: number) => void;
 }
 
-export const CartItemList = ({ cart, removeFromCart, updateQuantity }: Props) => {
+export const CartItemList = ({ key, cartItem, removeFromCart, updateQuantity }: Props) => {
+  const appliedDiscount = getAppliedDiscount(cartItem);
+
   return (
-    <div className="space-y-2">
-      {cart.map((item) => {
-        const appliedDiscount = getAppliedDiscount(item);
-        return (
-          <div key={item.product.id} className="flex justify-between items-center bg-white p-3 rounded shadow">
-            <div>
-              <span className="font-semibold">{item.product.name}</span>
-              <br />
+    <div key={key} className="flex justify-between items-center bg-white p-3 rounded shadow">
+      <div>
+        <span className="font-semibold">{cartItem.product.name}</span>
+        <br />
 
-              <span className="text-sm text-gray-600">
-                {item.product.price}원 x {item.quantity}
-                {appliedDiscount > 0 && <span className="text-green-600 ml-1">({(appliedDiscount * 100).toFixed(0)}% 할인 적용)</span>}
-              </span>
-            </div>
+        <span className="text-sm text-gray-600">
+          {cartItem.product.price}원 x {cartItem.quantity}
+          {appliedDiscount > 0 && <span className="text-green-600 ml-1">({(appliedDiscount * 100).toFixed(0)}% 할인 적용)</span>}
+        </span>
+      </div>
 
-            <div className="flex gap-1">
-              <Button label="-" onClick={() => updateQuantity(item.product.id, item.quantity - 1)}></Button>
-              <Button label="+" onClick={() => updateQuantity(item.product.id, item.quantity + 1)}></Button>
-              <Button label="삭제" variant="danger" onClick={() => removeFromCart(item.product.id)}></Button>
-            </div>
-          </div>
-        );
-      })}
+      <div className="flex gap-1">
+        <Button label="-" onClick={() => updateQuantity(cartItem.product.id, cartItem.quantity - 1)}></Button>
+        <Button label="+" onClick={() => updateQuantity(cartItem.product.id, cartItem.quantity + 1)}></Button>
+        <Button label="삭제" variant="danger" onClick={() => removeFromCart(cartItem.product.id)}></Button>
+      </div>
     </div>
   );
 };
